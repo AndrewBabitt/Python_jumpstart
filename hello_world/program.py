e24ca26f22eb57ce59deeebe65b1a9fdc46c7558
Andrew Babitt <andrew.babitt@gmail.com>
2019-03-10 11:27:35 -0600

    added hello world app

---
+++ hello_world/program.py
@@ -0,0 +1,8 @@
+print('---------------------------')
+print('        HELLO APP          ')
+print('---------------------------')
+
+user_input = input('What is your name?')
+greeting = 'Nice to meet you ' + user_input
+
+print(greeting)
